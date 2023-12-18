9cafa519026eca18ba9e0b18a7a827d93e17a37b
arcticmonkey07 <arcticmonkey07@gmail.com>
2023-12-18 14:37:42 +0300

    first commit

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Страница не найдена</h2>
+      <p>
+        Эту страницу не удалось найти. Возможно, просмотрите всех наших
+        <router-link to="/coaches">учителей</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
---
+++ src/store/modules/auth/index.js
@@ -0,0 +1,16 @@
+import mutations from '../auth/mutations.js';
+import actions from '../auth/actions.js';
+import getters from '../auth/getters.js';
+
+export default {
+  state() {
+    return {
+      userId: null,
+      token: null,
+      didAutoLogout: false,
+    };
+  },
+  mutations,
+  actions,
+  getters,
+};
